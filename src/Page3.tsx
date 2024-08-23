3dd0af4d0020ef7cf3fee446ea0b05699175fdcc
alex-romo <alejandro@mintitmedia.com>
2024-08-22 20:02:24 -0700

    about to break

--- src/Page3.tsx
+++ src/Page3.tsx
@@ -9,13 +9,12 @@ import {
   TableCell,
   TableRow,
 } from "src/components/ui/table"
-
 interface SheetProps {
   title: string;
   sheet: (string | number)[][];
 }
-
 import { marked } from 'marked';
+import { replaceUnderscoresWithSpaces } from "./lib/handleNames";
 
 // Reorganize nested data
 
@@ -49,14 +48,14 @@ function countEmptySpacesBelow(matrix: (string | number)[][], row: number, col: 
 const Page3: React.FC<SheetProps> = ({ sheet, title }) => {
   return <>
     <CardHeader>
-        <CardTitle>{title}</CardTitle>
+        <CardTitle>{replaceUnderscoresWithSpaces(title)}</CardTitle>
       </CardHeader>
       <CardContent className="flex gap-10 max-h-full overflow-hidden">
         <Table>
           <TableBody>
-          {sheet.map((item, i:number) => (
-            <TableRow key={`row-${i}`}>
-              {item.map((cell, j:number) => {
+          {sheet.map((row, i:number) => (
+            <TableRow key={`row-${i}`} className={`${i === 0 ? 'sticky top-0 bg-secondary' : ''}`}>
+              {row.map((cell, j:number) => {
                 return cell !== '' ? <TableCell key={`cell-${i}-${j}`} className="align-top" rowSpan={countEmptySpacesBelow(sheet, i, j)} dangerouslySetInnerHTML={{__html:marked(cell.toString()) }} /> : '';
 
               })}
