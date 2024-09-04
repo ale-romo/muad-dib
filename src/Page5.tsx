dcd5fe1b7d6c0dc33b4e905884891f2687f6251a
alex-romo <alejandro@mintitmedia.com>
2024-09-03 17:18:08 -0700

    feat: AI UI

--- src/Page5.tsx
+++ src/Page5.tsx
@@ -15,7 +15,7 @@ import {
 } from "src/components/ui/accordion";
 import { AiRmfProps } from './lib/ai-rmf-content';
 
-const SectionTitle = ({ children, top = 0 }: { children: React.ReactNode, top?: number }) => <h2  className="text-lg font-bold mb-6 pb-2 sticky bg-white" style={{ top }}>{children}</h2>
+const SectionTitle = ({ children }: { children: React.ReactNode, top?: number }) => <h2  className="text-lg font-bold p-5 z-10 sticky bg-white top-0">{children}</h2>
 
 const scrollToSection =  (id: string) => {
   const el = document.querySelector(id);
@@ -23,50 +23,60 @@ const scrollToSection =  (id: string) => {
 }
 
 const Page5: React.FC<AiRmfProps> = (props) => {
-  const menuHeightRef = useRef<HTMLDivElement | null>(null);
   const [currentSteps, setCurrentSteps] = useState<number[]>([3]);
 
-  return <Tabs defaultValue={Object.keys(props)[0]} className="m-5 overflow-hidden h-full">
-    <TabsList className="grid w-full grid-cols-2 sticky top-0" ref={menuHeightRef}>
+  return <Tabs defaultValue={Object.keys(props)[0]} className="h-full grid grid-rows-[auto,1fr]">
+    <TabsList className="grid grid-cols-2 m-5">
       {Object.entries(props).map(([key]) => (
         <TabsTrigger key={`menu-${key}`} value={key}>{key}</TabsTrigger>
       ))}
     </TabsList>
-    {Object.entries(props).map(([key, sections]) => (
-      <TabsContent value={key} key={`content-${key}`}>
-         <div className="gap-3 flex justify-end w-full items-center">
-          <label>{key}:</label>
-          {sections.map((section, i) => <Button key={`${section.title}-${i}`} variant="outline" onClick={() => scrollToSection(`#${section.title}`)}>{i+1}</Button>)}
-          <Button variant={currentSteps[0] === 3 ? 'secondary' : 'outline'} onClick={() => setCurrentSteps([3])}>Overview</Button>
-          <Button variant={currentSteps[0] === 4 ? 'secondary' : 'outline'} onClick={() => setCurrentSteps([4, 5])}>Tasks</Button>
-          <Button variant={currentSteps[0] === 6 ? 'secondary' : 'outline'} onClick={() => setCurrentSteps([6])}>References</Button>
-          <Button asChild>
-            <a href="worksheets" target="_blank">Download Worksheet</a>
-          </Button>
-        </div>
-        {sections.map((section, i) => (
-          <Accordion id={section.title} type="multiple" key={`overview-${i}`} defaultValue={['overview-steps-0-0']}>
-            <SectionTitle top={menuHeightRef.current?.clientHeight}>{section.title} {section.description}</SectionTitle>
-            {section.steps.map((step, j) => (
-              <AccordionItem value={`overview-steps-${i}-${j}`} key={`overview-steps-${j}`} className="[&:nth-child(odd)]:bg-gray-100/50 p-5">
-                <AccordionTrigger className="text-left">
-                  <div>
-                    <MdText className="text-xs text-gray-500">{`${step[0]}`}</MdText>
-                    <MdText className="text-xs font-bold mb-2 text-gray-500">{`${step[1]}`}</MdText>
-                    <MdText className="text-sm font-bold mb-2">{`${step[2]}`}</MdText>
-                  </div>
-                </AccordionTrigger>
-                <AccordionContent className="flex gap-10">
-                  <div className="">
-                  {currentSteps.map(currentStep => <MdText>{step[currentStep]}</MdText>)}
-                  </div>
-                </AccordionContent>
-              </AccordionItem>
-            ))}
-          </Accordion>
-        ))}
-      </TabsContent>
-    ))}
+    <div className="overflow-hidden">
+      {Object.entries(props).map(([key, sections]) => (
+        <TabsContent value={key} key={`content-${key}`} className="h-full">
+          <div className="flex flex-col h-full">
+            <div className="gap-3 flex justify-end items-center top-0 bg-white mx-5">
+              <label>{key}:</label>
+              {sections.map((section, i) => <Button key={`${section.title}-${i}`} variant="outline" onClick={() => scrollToSection(`#${section.title}`)}>{i+1}</Button>)}
+              <Button variant={currentSteps[0] === 3 ? 'secondary' : 'outline'} onClick={() => setCurrentSteps([3])}>Overview</Button>
+              <Button variant={currentSteps[0] === 4 ? 'secondary' : 'outline'} onClick={() => setCurrentSteps([4, 5])}>Tasks</Button>
+              <Button variant={currentSteps[0] === 6 ? 'secondary' : 'outline'} onClick={() => setCurrentSteps([6])}>References</Button>
+              <Button asChild>
+                <a href="worksheets" target="_blank">Download Worksheet</a>
+              </Button>
+            </div>
+            <div className="flex-grow overflow-y-scroll">
+              {sections.map((section, i) => (
+                <Accordion
+                  id={section.title}
+                  type="multiple"
+                  key={`overview-${i}`}
+                  defaultValue={['overview-steps-0-0']}
+                >
+                  <SectionTitle>{section.title}: {section.description}</SectionTitle>
+                  {section.steps.map((step, j) => (
+                    <AccordionItem value={`overview-steps-${i}-${j}`} key={`overview-steps-${j}`} className="[&:nth-child(odd)]:bg-gray-100/50 p-5">
+                      <AccordionTrigger className="text-left">
+                        <div>
+                          <MdText className="text-xs text-gray-500">{`${step[0]}`}</MdText>
+                          <MdText className="text-xs font-bold mb-2 text-gray-500">{`${step[1]}`}</MdText>
+                          <MdText className="text-sm font-bold mb-2">{`${step[2]}`}</MdText>
+                        </div>
+                      </AccordionTrigger>
+                      <AccordionContent className="flex gap-10">
+                        <div className="">
+                        {currentSteps.map(currentStep => <MdText key={`step-${currentStep}`}>{step[currentStep]}</MdText>)}
+                        </div>
+                      </AccordionContent>
+                    </AccordionItem>
+                  ))}
+                </Accordion>
+              ))}
+            </div>
+          </div>
+        </TabsContent>
+      ))}
+    </div>
   </Tabs>
 }
 
